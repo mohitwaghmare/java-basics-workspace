c8e289bb6ab3dff07624867ae8ad649c10fdd0c9
mohitwaghmare <mohit.waghmare@outlook.com>
2026-05-29 15:48:20 +0530

    Added SingleDimensionalArray.java example

---
+++ arrays/SingleDimensionalArray.java
@@ -0,0 +1,34 @@
+package arrays;
+
+// Java Single Dimensional Array Example
+// This program demonstrates arrays in Java.
+
+public class SingleDimensionalArray {
+
+    public static void main(String[] args) {
+
+        /*
+         * An array stores multiple values
+         * of the same data type.
+         */
+
+        // Creating an integer array
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        /*
+         * Array indexing starts from 0.
+         */
+
+        System.out.println("First Element: " + numbers[0]);
+
+        System.out.println("Second Element: " + numbers[1]);
+
+        System.out.println("Third Element: " + numbers[2]);
+
+        System.out.println("Fourth Element: " + numbers[3]);
+
+        System.out.println("Fifth Element: " + numbers[4]);
+
+        System.out.println("Single dimensional array example completed.");
+    }
+}
