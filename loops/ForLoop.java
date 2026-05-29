967049b61af1f09aea2ed3181f4705668586a6fe
mohitwaghmare <mohit.waghmare@outlook.com>
2026-05-29 15:26:10 +0530

    Added ForLoop.java example

---
+++ loops/ForLoop.java
@@ -0,0 +1,28 @@
+package loops;
+
+// Java For Loop Example
+// This program demonstrates for loop in Java.
+
+public class ForLoop {
+
+    public static void main(String[] args) {
+
+        /*
+         * A loop is used to repeat
+         * a block of code multiple times.
+         */
+
+        /*
+         * for loop syntax:
+         *
+         * for(initialization; condition; update)
+         */
+
+        for (int i = 1; i <= 5; i++) {
+
+            System.out.println("Count: " + i);
+        }
+
+        System.out.println("For loop example completed.");
+    }
+}
