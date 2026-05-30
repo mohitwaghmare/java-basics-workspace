f272667f2c5060c2d209475b42e2f7afe6cc737c
mohitwaghmare <mohit.waghmare@outlook.com>
2026-05-31 00:44:55 +0530

    Added MethodBasics.java example

---
+++ methods/MethodBasics.java
@@ -0,0 +1,29 @@
+package methods;
+
+// Java Method Basics Example
+// This program demonstrates basic methods in Java.
+
+public class MethodBasics {
+
+    /*
+     * A method is a block of code
+     * that performs a specific task.
+     */
+
+    // Creating a method
+    static void greetUser() {
+
+        System.out.println("Welcome to Java Methods!");
+    }
+
+    public static void main(String[] args) {
+
+        /*
+         * Calling the method.
+         */
+
+        greetUser();
+
+        System.out.println("Method basics example completed.");
+    }
+}
